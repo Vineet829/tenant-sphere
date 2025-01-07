ef9468a01115fd04e69709afd80a668548d5743c
Vineet827 <vineetsingh5987@gmail.com>
2025-01-08 00:18:33 +0530

    feat: add left navigation sidebar

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -2,6 +2,7 @@ import shockedFace from "@/../public/assets/icons/shocked face.svg";
 import Image from "next/image";
 import Link from "next/link";
 
+
 export default function NotFound() {
 	return (
 		<>
