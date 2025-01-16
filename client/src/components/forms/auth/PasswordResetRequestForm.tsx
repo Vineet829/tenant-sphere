574389adcedbd41ed141076abde311f8a573b8b0
Vineet827 <vineetsingh5987@gmail.com>
2025-01-16 23:35:18 +0530

    feat: add password reset request and confirm pages

--- client/src/components/forms/auth/PasswordResetRequestForm.tsx
+++ client/src/components/forms/auth/PasswordResetRequestForm.tsx
@@ -14,6 +14,7 @@ import { MailIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Spinner from "@/components/shared/Spinner";
 
+
 export default function PasswordResetRequestForm() {
 	const [resetPasswordRequest, { isLoading }] =
 		useResetPasswordRequestMutation();
