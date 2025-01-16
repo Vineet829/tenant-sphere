b939f52a5d47fa653463dfd3c356bed3b5c3b7dd
Vineet827 <vineetsingh5987@gmail.com>
2025-01-16 23:34:58 +0530

    feat: add password reset request and confirm pages

--- client/src/components/forms/auth/PasswordResetConfirmForm.tsx
+++ client/src/components/forms/auth/PasswordResetConfirmForm.tsx
@@ -14,6 +14,7 @@ import { FormFieldComponent } from "@/components/forms/FormFieldComponent";
 import { Button } from "@/components/ui/button";
 import Spinner from "@/components/shared/Spinner";
 
+
 export default function PasswordResetConfirmForm() {
 	const router = useRouter();
 	const { uid, token } = useParams();
