eb1bf1eb3988fa7f6f14a06476a6c0b82040cf60
Vineet827 <vineetsingh5987@gmail.com>
2025-01-17 16:05:25 +0530

    feat: show user details on profile page

--- client/src/components/profile/Posts.tsx
+++ client/src/components/profile/Posts.tsx
@@ -5,6 +5,7 @@ import MyPostCard from "../cards/MyPostCard";
 
 export default function Posts() {
 	return (
+		
 		<TabsContent value="posts">
 			<div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2">
 				<h3 className="h3-semibold dark:text-platinum"><MyPostCard/></h3>
