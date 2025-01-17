502152778dec5b0b5280e273db7a63ac5eed1246
Vineet827 <vineetsingh5987@gmail.com>
2025-01-17 21:13:42 +0530

    feat: add sign in with Google on client

--- client/src/components/shared/OauthButtons.tsx
+++ client/src/components/shared/OauthButtons.tsx
@@ -4,6 +4,7 @@ import { UseGoogle } from "@/utils";
 import OauthButton from "./OauthButton";
 
 export default function OauthButtons() {
+	
 	return (
 		<div className="mt-3 flex items-center justify-between gap-2">
 			<OauthButton provider="google" onClick={UseGoogle}>
