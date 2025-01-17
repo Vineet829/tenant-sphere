ff05e661c87688510776b47fd2db95d928c66cbd
Vineet827 <vineetsingh5987@gmail.com>
2025-01-17 22:12:39 +0530

    feat: add theme switcher component

--- client/src/components/ui/card.tsx
+++ client/src/components/ui/card.tsx
@@ -6,6 +6,7 @@ const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
+  
   <div
     ref={ref}
     className={cn(
--- client/src/components/ui/dropdown-menu.tsx
+++ client/src/components/ui/dropdown-menu.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils"
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
+
 const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger
 
 const DropdownMenuGroup = DropdownMenuPrimitive.Group
