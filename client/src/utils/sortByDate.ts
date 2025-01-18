b83b1e3243f4d47a8585f49db554a18d1c6cdda5
Vineet827 <vineetsingh5987@gmail.com>
2025-01-18 22:35:54 +0530

    feat: display paginated list of posts on homepage

--- client/src/utils/sortByDate.ts
+++ client/src/utils/sortByDate.ts
@@ -1,5 +1,6 @@
 export const sortByDateDescending = <T>(arr: T[], property: keyof T): T[] => {
 	return arr.slice().sort((a, b) => {
+		
 		return (
 			new Date(b[property] as unknown as string).getTime() -
 			new Date(a[property] as unknown as string).getTime()
