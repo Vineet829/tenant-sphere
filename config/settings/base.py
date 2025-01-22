226d315f304845d5b974485acd2757baaa36f59b
Vineet827 <vineetsingh5987@gmail.com>
2025-01-22 15:17:21 +0530

    feat: add production config files

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 APPS_DIR = BASE_DIR / "core_apps"
