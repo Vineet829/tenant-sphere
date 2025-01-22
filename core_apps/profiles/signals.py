f05cc7a2c868d58b4ac81d26b44d9239bba13548
Vineet827 <vineetsingh5987@gmail.com>
2025-01-22 22:38:19 +0530

    feat: add TimeStamped,Profile models and signals

--- core_apps/profiles/signals.py
+++ core_apps/profiles/signals.py
@@ -11,6 +11,7 @@ from core_apps.profiles.models import Profile
 logger = logging.getLogger(__name__)
 
 
+
 @receiver(post_save, sender=AUTH_USER_MODEL)
 def create_user_profile(
     sender: Type[Model], instance: Model, created: bool, **kwargs: Any
