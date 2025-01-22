22dcf07bb5ea8277b00b97ad95ba302387ad3353
Vineet827 <vineetsingh5987@gmail.com>
2025-01-22 22:55:40 +0530

    feat: customize djoser current user serializer

--- core_apps/users/serializers.py
+++ core_apps/users/serializers.py
@@ -7,6 +7,7 @@ from rest_framework import serializers
 User = get_user_model()
 
 
+
 class CreateUserSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
